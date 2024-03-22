c159312dc0d8bc72ee47cf48183481135753319a
patrice012 <patricedev02@gmail.com>
2024-03-22 01:05:04 +0000

    update code base

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,68 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
     },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      // colors: {
+      //   kalami: {
+      //     dark: "#000000",
+      //     yellow: "#FEC204",
+      //     "gray-light": "#a5b4cb",
+      //     "icon-gray": "#9CA3AF",
+      //     green: "#1c9368",
+      //     white: "#ffffff",
+      //     bg: "#FBFAF8",
+      //     "border-gray": "#D1D5DB",
+      //     "main-blue": "#1F2937",
+      //     blue: "#4284F4",
+      //     red: "#FF0000",
+      //     "bg-gray": "#F3F4F6",
+      //   },
+
+      //   "kalami-dark": {
+      //     "icon-gray": "#9CA3AF",
+      //     "border-gray": "#374151",
+      //     "main-blue": "#1F2937",
+      //     "white-100": "#E5E7EB",
+      //     "gray-400": "#9CA3AF",
+      //     "gray-800": "#1F2937",
+      //     "gray-850": "#1f293787",
+      //     "gray-900": "#111928",
+      //     "gray-300": "#D1D5DB",
+      //   },
+      // },
+    },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config
